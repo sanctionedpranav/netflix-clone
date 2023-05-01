0fe567d0c6e760bc15ad3e28b28f2c8b474fae36
Pranav <98287084+sanctionedpranav@users.noreply.github.com>
2023-05-01 12:32:36 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "firebase";
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9s0bguXjHbzt67XQ1GMu6gl4A1yBrvn0",
-    authDomain: "netflix-clone-d14ae.firebaseapp.com",
-    projectId: "netflix-clone-d14ae",
-    storageBucket: "netflix-clone-d14ae.appspot.com",
-    messagingSenderId: "536019190282",
-    appId: "1:536019190282:web:c75ea1199c7f62f81d55f3",
-    measurementId: "G-SJPEE3HX0P"
-};
+    apiKey: "AIzaSyBcjvnyLjmVygw002xkJuxJpf3jiZ74HcU",
+    authDomain: "netflix-clone-e969f.firebaseapp.com",
+    projectId: "netflix-clone-e969f",
+    storageBucket: "netflix-clone-e969f.appspot.com",
+    messagingSenderId: "485187235227",
+    appId: "1:485187235227:web:c0bc2fc55ad9f6f5bcc53e"
+  };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
